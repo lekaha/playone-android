95a33237fcbb1baba74c6a0ae59f94174acf68d6
Jieyi <pokkbaby@gmail.com>
2018-02-17 22:28:19 +0900

    Removed the redundant code.

--- ext/src/main/java/com/playone/mobile/ext/system/Time.kt
+++ ext/src/main/java/com/playone/mobile/ext/system/Time.kt
@@ -2,8 +2,5 @@ package com.playone.mobile.ext.system
 
 /**
  * An extension collections about the Time.
- *
- * @author  Jieyi Wu
- * @since   2018/02/17
  */
 val currentTime get() = System.currentTimeMillis()
