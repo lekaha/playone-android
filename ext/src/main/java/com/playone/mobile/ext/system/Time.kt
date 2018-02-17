6be3a102c16639d5627eff5607b61d8409a2fb36
Jieyi <pokkbaby@gmail.com>
2018-02-17 20:34:19 +0900

    Added the time extension.

---
+++ ext/src/main/java/com/playone/mobile/ext/system/Time.kt
@@ -0,0 +1,9 @@
+package com.playone.mobile.ext.system
+
+/**
+ * An extension collections about the Time.
+ *
+ * @author  Jieyi Wu
+ * @since   2018/02/17
+ */
+val currentTime get() = System.currentTimeMillis()
