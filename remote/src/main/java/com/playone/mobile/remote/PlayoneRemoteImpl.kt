06e1368189e1b6806ff00a50bbd3eb12bb9fdb5c
Jieyi <pokkbaby@gmail.com>
2018-02-23 00:01:50 +0900

    Removed the warning annotation. https://github.com/lekaha/playone-android/pull/24#discussion_r169853793

--- remote/src/main/java/com/playone/mobile/remote/PlayoneRemoteImpl.kt
+++ remote/src/main/java/com/playone/mobile/remote/PlayoneRemoteImpl.kt
@@ -11,7 +11,6 @@ import com.playone.mobile.remote.model.PlayoneModel
 import com.playone.mobile.remote.model.UserModel
 import io.reactivex.Single
 
-@Suppress("NOTHING_TO_INLINE")
 /**
  * Remote implementation for retrieving Playone instances. This class implements the
  * [com.playone.mobile.data.repository.PlayoneRemote] from the Data layer as it is that
