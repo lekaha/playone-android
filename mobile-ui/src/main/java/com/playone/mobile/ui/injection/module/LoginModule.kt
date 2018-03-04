7ec7246369d4b120000ff2574654f774a83dcbe2
lekaha <lekaha@gmail.com>
2018-03-05 00:49:48 +0900

    add: login module

--- mobile-ui/src/main/java/com/playone/mobile/ui/injection/module/LoginModule.kt
+++ mobile-ui/src/main/java/com/playone/mobile/ui/injection/module/LoginModule.kt
@@ -1,9 +1,14 @@
 package com.playone.mobile.ui.injection.module
 
+import com.playone.mobile.domain.interactor.auth.SignUpAndSignIn
+import com.playone.mobile.presentation.login.LoginPlayonePresenter
 import dagger.Module
+import dagger.Provides
 
 @Module
 class LoginModule {
 
-    //TODO(implementation
+    @Provides
+    internal fun provideLoginPresenter(signUpAndSignIn: SignUpAndSignIn) =
+        LoginPlayonePresenter(signUpAndSignIn)
 }
