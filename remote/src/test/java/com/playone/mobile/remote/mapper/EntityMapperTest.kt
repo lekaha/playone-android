dbb97fa7766549d01cfab8e5ddfc5b7a2202f9d0
Jieyi <pokkbaby@gmail.com>
2018-03-06 02:57:45 +0900

    Added mapper test code.

---
+++ remote/src/test/java/com/playone/mobile/remote/mapper/EntityMapperTest.kt
@@ -0,0 +1,59 @@
+package com.playone.mobile.remote.mapper
+
+import com.playone.mobile.data.model.PlayoneEntity
+import com.playone.mobile.remote.model.UserModel
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.test.assertEquals
+
+@RunWith(JUnit4::class)
+class EntityMapperTest {
+
+    private lateinit var playoneEntityMapper: PlayoneEntityMapper
+    private lateinit var userEntityMapper: UserEntityMapper
+
+    @Before
+    fun setup() {
+        playoneEntityMapper = PlayoneEntityMapper()
+        userEntityMapper = UserEntityMapper()
+    }
+
+    @Test
+    fun playoneMapFromRemoteToData() {
+        val entity = PlayoneEntity()
+        val model = playoneEntityMapper.mapFromData(entity)
+
+        assertEquals(entity.id, model.id)
+        assertEquals(entity.name, model.name)
+        assertEquals(entity.description, model.description)
+        assertEquals(entity.date, model.date)
+        assertEquals(entity.updated, model.updated)
+        assertEquals(entity.address, model.address)
+        assertEquals(entity.longitude, model.longitude)
+        assertEquals(entity.latitude, model.latitude)
+        assertEquals(entity.limit, model.limit)
+        assertEquals(entity.level, model.level)
+        assertEquals(entity.host, model.host)
+        assertEquals(entity.userId, model.userId)
+    }
+
+    @Test
+    fun playoneMapFromDataToRemote() {
+        val model = UserModel()
+        val entity = userEntityMapper.mapToData(model)
+
+        assertEquals(entity.id, model.id)
+        assertEquals(entity.name, model.name)
+        assertEquals(entity.email, model.email)
+        assertEquals(entity.pictureURL, model.pictureURL)
+        assertEquals(entity.description, model.description)
+        assertEquals(entity.grade, model.grade)
+        assertEquals(entity.deviceToken, model.deviceToken)
+        assertEquals(entity.age, model.age)
+        assertEquals(entity.level, model.level)
+        assertEquals(entity.years, model.years)
+        assertEquals(entity.teams, model.teams)
+    }
+}
