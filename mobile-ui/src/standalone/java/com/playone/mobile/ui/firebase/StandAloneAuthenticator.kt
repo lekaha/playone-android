9cedfd0a3888fbd7934eefa62c4fc2e0119e27df
Jieyi <pokkbaby@gmail.com>
2018-03-08 00:37:13 +0900

    Merged the newest commit and resolved the conflict.

--- mobile-ui/src/standalone/java/com/playone/mobile/ui/firebase/StandAloneAuthenticator.kt
+++ mobile-ui/src/standalone/java/com/playone/mobile/ui/firebase/StandAloneAuthenticator.kt
@@ -6,16 +6,19 @@ import com.playone.mobile.domain.Credential
 import com.playone.mobile.domain.model.User
 
 class StandAloneAuthenticator : Authenticator() {
-
     val mapper = FirebaseUserMapper()
 
     override fun signUp(email: String, password: String, callback: AuthResultCallBack) = TODO()
 
     override fun signIn(credential: Credential<*>, callback: AuthResultCallBack) = TODO()
 
+    override fun signInAnonymously(callback: AuthResultCallBack) = TODO()
+
     override fun signOut(callback: AuthResultCallBack) = TODO()
 
     override fun isSignedIn() = false
+
+    override fun isVerifiedEmail() = TODO()
 
     class FirebaseUserMapper {
 
