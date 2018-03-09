efa340c03b897fd652e5e60af3b3f9a8722e101a
lekaha <lekaha@gmail.com>
2018-03-10 00:42:48 +0900

    feat: go to PlayoneActivity after sign in/up

---
+++ mobile-ui/src/main/java/com/playone/mobile/ui/playone/PlayoneActivity.kt
@@ -0,0 +1,10 @@
+package com.playone.mobile.ui.playone
+
+import com.playone.mobile.ui.BaseActivity
+import com.playone.mobile.ui.R
+
+class PlayoneActivity : BaseActivity() {
+
+    override fun getLayoutId() = R.layout.activity_playone
+
+}
