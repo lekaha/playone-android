2210d35ab41253150de124a8ddbf15216d137179
lekaha <lekaha@gmail.com>
2018-03-10 23:06:45 +0900

    feat: NotValidVar for checking invalid valid when getting var

---
+++ ext/src/main/java/com/playone/mobile/common/NotValidVar.kt
@@ -0,0 +1,18 @@
+package com.playone.mobile.common
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+private class NotValidVar<T : Any>(private val invalidVal: T) : ReadWriteProperty<Any?, T> {
+    private var value: T = invalidVal
+
+    public override fun getValue(thisRef: Any?, property: KProperty<*>): T =
+        if (value != invalidVal)
+            value
+        else
+            throw IllegalStateException("Property ${property.name} should be initialized before get.")
+
+    public override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = value
+    }
+}
