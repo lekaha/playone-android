7f8bd7017efff5ee4343d50145d789f864d0acb2
Jieyi <pokkbaby@gmail.com>
2018-03-10 13:23:35 +0900

    Added the retrieving the play list on the domain layer.

---
+++ domain/src/main/java/com/playone/mobile/domain/interactor/playone/GetPlayoneList.kt
@@ -0,0 +1,22 @@
+package com.playone.mobile.domain.interactor.playone
+
+import com.playone.mobile.domain.executor.PostExecutionThread
+import com.playone.mobile.domain.executor.ThreadExecutor
+import com.playone.mobile.domain.interactor.SingleUseCase
+import com.playone.mobile.domain.model.Playone
+import com.playone.mobile.domain.repository.PlayoneRepository
+import io.reactivex.Single
+
+/**
+ * Use case used for retrieving a [List] of [com.playone.mobile.domain.model.Playone]
+ * instances from the [com.playone.mobile.domain.repository.PlayoneRepository]
+ */
+open class GetPlayoneList constructor(
+    private val repository: PlayoneRepository,
+    threadExecutor: ThreadExecutor,
+    postExecutionThread: PostExecutionThread
+) : SingleUseCase<List<Playone>, Int>(threadExecutor, postExecutionThread) {
+
+    public override fun buildUseCaseObservable(params: Int?): Single<List<Playone>> =
+        repository.getPlayoneList(params ?: -1)
+}
