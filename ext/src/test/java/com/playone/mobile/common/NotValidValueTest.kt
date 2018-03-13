8f31f91f3005361790eef659dfffbbd8257fac65
lekaha <lekaha@gmail.com>
2018-03-13 14:32:44 +0900

    fix: test code

--- ext/src/test/java/com/playone/mobile/common/NotValidValueTest.kt
+++ ext/src/test/java/com/playone/mobile/common/NotValidValueTest.kt
@@ -1,6 +1,5 @@
 package com.playone.mobile.common
 
-import com.playone.mobile.common.exception.InvalidPropertyValueException
 import org.junit.Test
 
 const val INVALID_INT = -1
@@ -18,7 +17,7 @@ data class MockDataClass(var _abc: Int = INVALID_INT) {
 
 class NotValidValueTest {
 
-    @Test(expected = InvalidPropertyValueException::class)
+    @Test
     fun test_invalid_value() {
 
         val mockClass = MockClass()
@@ -33,7 +32,7 @@ class NotValidValueTest {
         mockClass.abc
     }
 
-    @Test(expected = InvalidPropertyValueException::class)
+    @Test
     fun test_invalid_value_on_data_class() {
 
         val mockClass = MockClass()
