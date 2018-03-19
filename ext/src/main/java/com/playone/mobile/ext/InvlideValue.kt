65bfc3e1918292b83fc7e03e7b677e3f550bde1a
Jieyi <pokkbaby@gmail.com>
2018-03-19 19:13:38 +0900

    Modified the default string.

--- ext/src/main/java/com/playone/mobile/ext/InvlideValue.kt
+++ ext/src/main/java/com/playone/mobile/ext/InvlideValue.kt
@@ -3,4 +3,4 @@ package com.playone.mobile.ext
 const val DEFAULT_INT = -1
 const val DEFAULT_LONG = -1L
 const val DEFAULT_DOUBLE = -1.0
-const val DEFAULT_STR = "invalidString"
+const val DEFAULT_STR = "<THIS IS INVALID STRING>"
