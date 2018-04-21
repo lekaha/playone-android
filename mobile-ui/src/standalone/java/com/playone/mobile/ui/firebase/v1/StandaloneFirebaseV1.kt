63633d9a7e2080f4077bbcb4d875d668df002543
lekaha <lekaha@gmail.com>
2018-04-22 01:26:18 +0900

    fix: standalone build error

--- mobile-ui/src/standalone/java/com/playone/mobile/ui/firebase/v1/StandaloneFirebaseV1.kt
+++ mobile-ui/src/standalone/java/com/playone/mobile/ui/firebase/v1/StandaloneFirebaseV1.kt
@@ -21,14 +21,14 @@ class StandaloneFirebaseV1 : PlayoneFirebase() {
     ) = TODO()
 
     override fun createPlayone(
-        userId: Int,
+        playoneId: String,
         model: PlayoneModel,
         callback: OperationResultCallback,
         errorCallback: FirebaseErrorCallback
     ) = TODO()
 
     override fun updatePlayone(
-        id: Int,
+        id: String,
         model: PlayoneModel,
         callback: OperationResultCallback,
         errorCallback: FirebaseErrorCallback
@@ -47,7 +47,7 @@ class StandaloneFirebaseV1 : PlayoneFirebase() {
     ) = TODO()
 
     override fun obtainPlayoneDetail(
-        userId: Int,
+        userId: String,
         callback: (model: PlayoneModel?) -> Unit,
         errorCallback: FirebaseErrorCallback
     ) = TODO()
