a990d7d044d51e3cc69de14dec864f78d5578650
lekaha <lekaha@gmail.com>
2018-07-19 23:38:47 +0900

    feat: start developing user profile

---
+++ mobile-ui/src/main/java/com/playone/mobile/ui/user/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.playone.mobile.ui.user
+
+import com.playone.mobile.ui.BaseActivity
+
+class ProfileActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
